48d4d7c37a9ce95887c7aa9d465cb908f57e20ce
Nourhan <nourhan.aboelenen@linkdev.com>
2025-06-24 14:12:42 +0300

    basic components and style added

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <img src="logo.png" alt="My Store Logo"/>
+        <span style={{fontSize: "1.5rem"}}>My Store</span>
+    </header>
+  )
+}
