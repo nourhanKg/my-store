4ccb868499b32a234e3ce7951f960a30e3ac8e55
Nourhan <nourhan.aboelenen@linkdev.com>
2025-06-27 00:25:08 +0300

    update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 import react from '@astrojs/react';
-
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: node({ // <--- Configure the Node.js adapter
-    mode: 'standalone', // Or 'middleware' depending on your setup
-  }),
+  adapter: netlify(),
+  // adapter: node({ // <--- Configure the Node.js adapter
+  //   mode: 'standalone', // Or 'middleware' depending on your setup
+  // }),
 });
